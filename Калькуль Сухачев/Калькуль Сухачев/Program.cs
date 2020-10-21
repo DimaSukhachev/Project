4c125795eca9eb92a58a1f5599b72770e7046c60
DimaSukhachev <72729476+DimaSukhachev@users.noreply.github.com>
2020-10-21 14:27:34 +0500

    Add files via upload

---
+++ Калькуль Сухачев/Калькуль Сухачев/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Калькуль_Сухачев
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            char oper = Convert.ToChar(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            if (oper == '+')
+            {
+                Console.WriteLine(x + y);
+            }    
+            else if (oper == '-')
+            {
+                Console.WriteLine(x-y);
+            }
+            else if (oper == '*')
+            {
+                Console.WriteLine(x * y);
+            }
+            else if (oper == ':')
+            {
+                Console.WriteLine(x / y);
+            }
+            Console.ReadKey(true);324
+
+        }
+    }
+}

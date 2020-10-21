ae3551dbc978037aba46f6c6c2024d4c8aa2e145
DimaSukhachev <72729476+DimaSukhachev@users.noreply.github.com>
2020-10-21 13:22:45 +0500

    Add files via upload

---
+++ калькулятор сухачев/калькулятор сухачев/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables and then initialize to zero.
+            int num1 = 0; int num2 = 0;
+
+            // Display title as the C# console calculator app.
+            Console.WriteLine("Консоль Калькулятор в C#\r");
+            Console.WriteLine("------------------------\n");
+
+            // Ask the user to type the first number.
+            Console.WriteLine("Введите число, и затем нажмите Enter");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask the user to type the second number.
+            Console.WriteLine("Введито 2 число, и затем нажмите Enter");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask the user to choose an option.
+            Console.WriteLine("Сделайте выбор из данного листа:");
+            Console.WriteLine("\ta - Сложение");
+            Console.WriteLine("\ts - Вычитание");
+            Console.WriteLine("\tm - Умножение");
+            Console.WriteLine("\td - Деление");
+            Console.Write("Ваш выбор? ");
+
+            // Use a switch statement to do the math.
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"Ваш результат: {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "s":
+                    Console.WriteLine($"Ваш результат: {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "m":
+                    Console.WriteLine($"Ваш результат: {num1} * {num2} = " + (num1 * num2));
+                    break;
+                case "d":
+                    Console.WriteLine($"Ваш результат: {num1} / {num2} = " + (num1 / num2));
+                    break;
+            }
+            // Wait for the user to respond before closing.
+            Console.Write("Нажмите любую кнопку чтобы закрыть консоль...");
+            Console.ReadKey();
+        }
+    }
+}
